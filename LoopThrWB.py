ea401da47ccac800d9973a6bd887ff48dd8af034
darsigangothri06 <darsigangothri@gmail.com>
2021-12-23 18:06:54 +0530

    Add: Adding data into excel file

--- LoopThrWB.py
+++ LoopThrWB.py
@@ -5,7 +5,8 @@ from openpyxl.utils import get_column_letter
 wb = openpyxl.load_workbook("MarksList.xlsx")
 ws = wb.active  # active sheetname
 
-for row in range(1,6):
-    for col in range(1,2):
+for row in range(7,15):
+    for col in range(1,4):
         char = get_column_letter(col)
-        print(ws[char + str(row)].value)
+        ws[char + str(row)] = char + str(row)
+wb.save("MarksList.xlsx")
