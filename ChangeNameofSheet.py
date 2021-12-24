e8723a8dd45087be0578248d10463489dd57b61c
darsigangothri06 <darsigangothri@gmail.com>
2021-12-24 08:04:13 +0530

    Add: Changing colors of sheets

--- ChangeNameofSheet.py
+++ ChangeNameofSheet.py
@@ -8,4 +8,8 @@ ws.title = "MASTERSHEET"
 ws1 = wb.active
 print(ws1)  # MASTERSHEET
 
+# changing the color of sheet
+ws1.sheet_properties.tabColor = "85E51D"
+ws2 = wb['Master2']
+ws2.sheet_properties.tabColor = "e5691d"
 wb.save("Inserting.xlsx")
