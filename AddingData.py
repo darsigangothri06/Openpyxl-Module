797bccabe4571572734a917fbd993d14ba679e84
darsigangothri06 <darsigangothri@gmail.com>
2021-12-26 08:04:11 +0530

    Add: Displaying DATA

--- AddingData.py
+++ AddingData.py
@@ -13,4 +13,11 @@ ws.insert_rows(0)
 ws.merge_cells("A1:J1")
 ws.cell(row = 1, column=1).value = "NUMBERS"
 
+# Displaying data of Excel cells
+rowStr = ''
+for row in ws.iter_rows(min_row = 1, max_row=ws.max_row, max_col=ws.max_column):
+    for cell in row:
+        rowStr += "{:<5}".format(str(cell.value) + " ")
+    rowStr += '\n'
+print(rowStr)
 wb.save("Numbers.xlsx")
