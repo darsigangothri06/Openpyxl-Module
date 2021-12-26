3ff75c5ab9e8a3cd6bab8d1dd180b01113fd06ca
darsigangothri06 <darsigangothri@gmail.com>
2021-12-26 15:05:27 +0530

    Add: Moving cells

--- MoveAndCopy.py
+++ MoveAndCopy.py
@@ -1,29 +1,17 @@
 import openpyxl
 
-wb = openpyxl.load_workbook("Numbers.xlsx")
+filename = "MovePractice.xlsx"
+wb = openpyxl.Workbook()
 ws = wb.active
 
-# Set data into file
-def SET(ws):
-    cnt = 1
-    for row in range(1,11):
-        for col in range(1,11):
-            ws.cell(row = row, column=col).value = cnt
-            cnt += 1
-            
-# Displaying data
-def PRINT(ws):
+def print_rows(ws):
     rowS = ''
     for row in ws.iter_rows(min_row=1, max_col=ws.max_column, max_row=ws.max_row):
         for cell in row:
-            rowS += "{:<3}".format(str(cell.value) + " ")
+            rowS += "{:<3}".format(str(cell.value) + '  ')
         rowS += '\n'
-    print(rowS + '\n')
-
-# Move Range method
-ws.move_range("A1:J10", rows=10)
-
-SET(ws)
-PRINT(ws)
-
-wb.save("Numbers.xlsx")
+    print(rowS)
+    print('*'*20)
+    
+# def SetValues(ws):
+ws.delete_cols(1,100)
